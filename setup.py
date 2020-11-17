87f9de708e90864cede260c8c2e104e409238620
JaccoPrins <s1118825@student.windesheim.nl>
2020-11-17 16:26:34 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tool Textanalysis',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/JaccoPrins/Tool_Textanalysis',
+    license='',
+    author='Jacco Prins',
+    author_email='jaccoprins98@gmail.com',
+    description='Text analysis by NLTK'
+)
